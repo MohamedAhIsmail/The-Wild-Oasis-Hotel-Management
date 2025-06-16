bf312a5bb513112642d08a95348c3f53bc8f1c94
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-06-16 10:21:27 +0300

    connect app with supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://wbedqfotipczlixysdgy.supabase.co";
+
+//RSL is enabled so you can use the api key here => do not worry
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndiZWRxZm90aXBjemxpeHlzZGd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwNDEyMTIsImV4cCI6MjA2NTYxNzIxMn0.q30v8jq0d42UD7qXuZ29IKDKVYVYS29xE_Ec7Md3BA4";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
