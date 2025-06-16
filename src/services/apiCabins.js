718ff0037b5aea206c39caca402de1872c3b5b3a
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-06-16 13:47:03 +0300

    handel delete cabin

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,3 +9,11 @@ export async function getCabins() {
 
   return data;
 }
+export async function deletCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be deleted");
+  }
+  return data;
+}
