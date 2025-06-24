e32192b5008d92f806712fd39e09ae7c7cc169c5
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-06-24 10:53:33 +0300

    handle log in

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading: isLogining } = useMutation({
+    mutationFn: ({ email, password }) => loginApi({ email, password }),
+    onSuccess: (user) => {
+      toast.success("Login in Successfully");
+      navigate("/");
+    },
+    onError: (error) => {
+      console.log("Error", error);
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { login, isLogining };
+}
