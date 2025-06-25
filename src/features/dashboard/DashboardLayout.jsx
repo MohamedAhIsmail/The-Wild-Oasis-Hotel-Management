cf0eb5a5a22830e0a565b6192cff75170dff1c5d
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-06-25 13:41:10 +0300

    created functions and computed data for dashboard statistics

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,23 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isLoading: isLoadingBookings, bookings } = useRecentBookings();
+  const { isLoading: isLoadingStays, stays } = useRecentStays();
+
+  if (isLoadingBookings || isLoadingStays) return <Spinner />;
+  console.log(bookings);
+  console.log(stays);
+
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Today's activity</div>
+      <div>Chart stay durations</div>
+      <div>Chart sals</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -52,10 +52,12 @@ export async function getBooking(id) {
 }
 
 // Returns all BOOKINGS that are were created after the given date. Useful to get bookings created in the last 30 days, for example.
+
+//Date: ISO String
 export async function getBookingsAfterDate(date) {
   const { data, error } = await supabase
     .from("bookings")
-    .select("created_at, totalPrice, extrasPrice")
+    .select("created_at, totalPrice, extraPrice")
     .gte("created_at", date)
     .lte("created_at", getToday({ end: true }));
 
